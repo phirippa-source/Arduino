7eb7707e7b66d839bc914ac5ec30908f5f058139
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2024-11-30 20:52:26 +0900

    Add files via upload

---
+++ Arduino_DHT11_BH1750.ino
@@ -0,0 +1,30 @@
+#include <Wire.h>
+#include <BH1750.h>
+#include "DHT.h"
+#define DHTPIN 4 
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+BH1750 lightMeter;
+
+void setup() {
+   Serial.begin(9600);
+   Wire.begin();
+   lightMeter.begin();
+   dht.begin();
+}
+
+void loop() {
+   float lux = lightMeter.readLightLevel();
+   float t = dht.readTemperature();
+   float h = dht.readHumidity();
+   if (isnan(t) || isnan(h)) {
+      Serial.println(F("Failed to read from DHT sensor!"));
+      return;
+   }
+   String payload = "{\"Temp\":" + String(t,1)
+                  + ",\"Humi\":" + String(h, 1)
+                  + ",\"Lux\":" + String(lux) + "}" + "\n";
+   Serial.print(payload); 
+   delay(1000);
+}
