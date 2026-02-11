c09349fe01bbdeca071f01740c5f36f0b2069fa1
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2026-02-12 07:43:28 +0900

    Create get_time_ntp_v1.2.ino

---
+++ UNO R4 WiFi/get_time_ntp_v1.2.ino
@@ -0,0 +1,36 @@
+#include <WiFiS3.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
+const char* ssid     = "Home2G";
+const char* password = "730124go";
+WiFiUDP udp;
+
+const long utcOffsetSeconds = 9L * 3600L;
+const unsigned long updateIntervalMs = 60UL * 1000UL; // 60초
+NTPClient timeClient(udp, "kr.pool.ntp.org", utcOffsetSeconds, updateIntervalMs);
+
+char daysOfTheWeek[7][12] = {
+  "Sunday","Monday","Tuesday","Wednesday","Thurday","Friday","Saturday"};
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) { } // (필요 시) 시리얼 연결 대기
+
+  Serial.println("WiFi connecting...");
+  while(WiFi.begin(ssid, password) != WL_CONNECTED) {
+    delay(500); Serial.print(".");
+  }
+  Serial.println("\nWiFi connected!");
+
+  while(WiFi.localIP()==IPAddress(0,0,0,0)){delay(200);}
+  timeClient.begin();
+}
+
+void loop() {
+  timeClient.update();
+  Serial.print( daysOfTheWeek[ timeClient.getDay( ) ] );
+  Serial.print(" ");
+  Serial.println( timeClient.getFormattedTime( ) );
+
+  delay(1000);
+}
