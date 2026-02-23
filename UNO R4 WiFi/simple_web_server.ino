2c71fbd0d0cf24c864f4057f7c707f57d8a22ffc
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2026-02-23 14:06:26 +0900

    Add simple web server functionality
    Implement a simple web server using UNO R4 WiFi.

---
+++ UNO R4 WiFi/simple_web_server.ino
@@ -0,0 +1,42 @@
+#include <WiFiS3.h>  // UNO R4 WiFi 전용 WiFi 라이브러리
+const char* ssid = "Ria2G";
+const char* password = "2026gogo";
+WiFiServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) { } // (필요 시) 시리얼 연결 대기
+
+  Serial.println("WiFi connecting...");
+  while(WiFi.begin(ssid, password) != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected!");
+
+  while( WiFi.localIP() == IPAddress(0,0,0,0)){
+    delay(200);
+  }
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+  Serial.println("웹 서버 시작 (포트 80)");
+  Serial.println("브라우저에서 위 IP 주소로 접속하세요.");
+}
+
+void loop() {
+  WiFiClient client = server.available();
+
+  if (client) {
+    Serial.println("클라이언트 접속");
+    while (client.connected()) {
+      //Serial.println("클라이언트와 연결됨");
+      //client.println("<h1>Hello from Arduino UNO R4 Wifi</h1>");
+      // do Something!
+      break;
+    }
+  delay(1); // 응답 전송 안정화
+  client.stop(); //연결 종료
+  Serial.println("클라이언트 종료");
+  }
+}
